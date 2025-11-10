c63516d64ca0446b6be3dc6f702c15d4bda26894
Brann4 <dennis.brama98@gmail.com>
2025-11-10 14:37:21 -0500

    Agregando metodos de login, store y service 1/2

--- src/app/pages/auth/login/components/login.component.ts
+++ src/app/pages/auth/login/components/login.component.ts
@@ -1,7 +1,9 @@
+import { TokenService } from '@/layout/service/token.service';
 import { PrimeModules } from '@/utils/PrimeModule';
 import { Component, inject, signal } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthStore } from '../stores/AuthStore';
 
 @Component({
     selector: 'app-login',
@@ -10,13 +12,15 @@ import { Router } from '@angular/router';
     templateUrl: './login.component.html'
 })
 export class Login {
+    tokenStorage = inject(TokenService)
+    authService = inject(AuthStore);
+
     frmLogin: FormGroup;
     fb = inject(FormBuilder);
     router = inject(Router);
     isSubmitting = signal<boolean>(false);
 
     email: string = '';
-
     password: string = '';
 
     checked: boolean = false;
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    URL: 'http://40.122.134.6:8031/api'
-    //URL: 'http://localhost:8183/api'
+    //URL: 'http://40.122.134.6:8031/api'
+    URL: 'http://localhost:8183/api'
 };
